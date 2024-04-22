e08d4ecc4027d77cad0aab7910dd5734cef65c30
Olegas1995 <163689467+Olegas1995@users.noreply.github.com>
2024-04-22 18:13:57 +0300

    Update HW_padstart_padend.js

--- HW_padstart_padend.js
+++ HW_padstart_padend.js
@@ -1,1 +1,32 @@
 
+function padEnd(str, symbol, length) {
+  if (str.length >= length) {
+      return str;
+  }
+  
+  const diff = length - str.length;
+  const pad = symbol.repeat(diff);
+  
+  return str + pad;
+}
+
+
+function padStart(str, symbol, length) {
+  if (str.length >= length) {
+      return str;
+  }
+  
+  const diff = length - str.length;
+  const pad = symbol.repeat(diff);
+  
+  return pad + str;
+}
+
+
+function pad(str, symbol, length, isStart) {
+  if (isStart) {
+      return padStart(str, symbol, length);
+  } else {
+      return padEnd(str, symbol, length);
+  }
+}
